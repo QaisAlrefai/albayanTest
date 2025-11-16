aacf274982d13bc60bd557eff8eb4bf2c9532f2e
Qais <58926070+QaisAlrefai@users.noreply.github.com>
2025-11-16 20:06:28 +0300

    Add PowerShell script for virtual environment setup
    Introduces albayan_env.ps1 to create or activate a Python virtual environment and install requirements. The script prompts the user for action and handles both environment creation and activation.

---
+++ albayan_env.ps1
@@ -0,0 +1,27 @@
+$Host.UI.RawUI.WindowTitle = "Virtual environment"
+
+
+Set-Location -Path $PSScriptRoot
+
+
+$choice = Read-Host "Do you want to Create or Activate the environment? (C/A)"
+
+if ($choice -ieq "C") {
+    Write-Host "Creating virtual environment..."
+    Start-Process -Wait "py" -ArgumentList "-3.13", "-m", "venv", "albayan_env"
+
+    
+    Write-Host "Activating environment and installing requirements..."
+    Start-Process "powershell.exe" -ArgumentList "-NoExit", "-Command",
+    "Set-Location '$PSScriptRoot'; & 'albayan_env\Scripts\Activate.ps1'; pip install -r requirements.txt"
+}
+elseif ($choice -ieq "A") {
+    Write-Host "Activating existing environment..."
+    Start-Process "powershell.exe" -ArgumentList "-NoExit", "-Command",
+    "Set-Location '$PSScriptRoot'; & 'albayan_env\Scripts\Activate.ps1'"
+}
+else {
+    Write-Host "Invalid choice. Please enter C or A." -ForegroundColor Red
+}
+
+exit
